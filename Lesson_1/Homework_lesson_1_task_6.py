32e94edd5c8e89033c79e0a670fc776bf66add4b
Mmak79 <99182063+Mmak79@users.noreply.github.com>
2022-02-10 23:45:14 +0700

    remove commented

--- Lesson_1/Homework_lesson_1_task_6.py
+++ Lesson_1/Homework_lesson_1_task_6.py
@@ -2,23 +2,10 @@ revenue = int(input("Enter revenue: "))
 costs = int(input("Enter costs: "))
 if costs < revenue:
     profit = int(revenue) - int(costs)
-    # print(int(profit))
+
     if profit > 0:
         profit_result = int((profit / revenue) * 100)
         print(profit_result)
         num_of_emp = int(input("Enter number of employees: "))
         profit_result_for_1 = profit / num_of_emp
         print("Your profit_result_for_1: ", profit_result_for_1)
-
-
-
-
-
-
-# if financial_results > 0
-#
-#     print("you financial results positive and = ", financial_results)
-# if financial_results < 0:
-#     print("you financial results negative and = ", financial_results)
-# if financial_results == 0:
-#     print("you financial results = ", financial_results)
